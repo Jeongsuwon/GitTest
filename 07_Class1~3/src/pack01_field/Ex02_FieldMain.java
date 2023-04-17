6ce942db735fcff2418e7c809fbeead7fad8eda1
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-04-17 17:03:55 +0900

    Delete 07_Class1~3 directory

--- 07_Class1~3/src/pack01_field/Ex02_FieldMain.java
+++
@@ -1,9 +0,0 @@
-package pack01_field;
-
-public class Ex02_FieldMain {
-	public static void main(String[] args) {
-		Ex02_Field food = new Ex02_Field();
-		
-	}
-	
-}
--- 07_Class1~3/src/pack03_constructor/Ex01_ConsMain.java
+++
@@ -1,9 +0,0 @@
-package pack03_constructor;
-
-public class Ex01_ConsMain {
-
-	public static void main(String[] args) {
-		Ex01_Cons cons = new Ex01_Cons();
-	}
-
-}
